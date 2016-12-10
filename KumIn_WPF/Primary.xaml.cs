e0ee4e3c4da62da2d515360a779388f6f032f727
srinath99 <srinath99@gmail.com>
2016-12-09 16:40:58 -0800

    Revert "Cleaning up"

--- KumIn_WPF/Primary.xaml.cs
+++ KumIn_WPF/Primary.xaml.cs
@@ -326,11 +326,13 @@ namespace KumIn_WPF
             string barcode = "";
 
 
-            IList<IList<Object>> values = getSpreadsheetInfo(spreadsheetId, "DB-Master!A1:AI");
+            IList<IList<Object>> values = getSpreadsheetInfo(spreadsheetId, "Sheet1!A1:I");
             int rowNum = 1;
 
             if (values != null && values.Count > 0)
             {
+
+
                 foreach (var row in values)
                 {
                     if (row[4].ToString() == checkValues[0])
@@ -340,13 +342,13 @@ namespace KumIn_WPF
                             if (row[1].ToString().ToUpper() == checkValues[1].ToUpper()
                             && row[0].ToString().ToUpper() == checkValues[2].ToUpper())
                             {
-                                range = "DB-Master!A" + rowNum.ToString() + ":" + "AAA" + rowNum.ToString();
+                                range = "Sheet1!A" + rowNum.ToString() + ":" + "AAA" + rowNum.ToString();
                                 break; // additional autherntiacation
                             }
                         }
                         else
                         {
-                            range = "DB-Master!A" + rowNum.ToString() + ":" + "AAA" + rowNum.ToString();
+                            range = "Sheet1!A" + rowNum.ToString() + ":" + "AAA" + rowNum.ToString();
                             break;
                         }
                     }
@@ -354,37 +356,18 @@ namespace KumIn_WPF
                         rowNum++;
                 }
             }
-            
+
             values = getSpreadsheetInfo(spreadsheetId, range);
-            
+
+
             foreach (var row in values)
             {
                 lastName = row[7].ToString();
                 firstName = row[5].ToString();
+                lastDayIn = row[8].ToString(); //check
                 barcode = row[4].ToString(); // check             
             }
-            //get rowNum from Assignment Record spreadsheet
-            IList<IList<Object>> values2 = getSpreadsheetInfo(spreadsheetId2, "Test!A1:B");
-            int rowNum2 = 1;
-            string range2 = "";
-            foreach (var row in values2)
-            {
-                if (row[1].ToString() == firstName 
-                        && row[0].ToString() == lastName)
-                {
-                    range2 = "Test!A" + rowNum2.ToString() + ":" + "AAA" + rowNum2.ToString();
-                    break;
-                }
-                else
-                {
-                    rowNum2++;
-                }
-            }
-            values2 = getSpreadsheetInfo(spreadsheetId2, range2);
-            foreach (var row in values2)
-            {
-                lastDayIn = row[24].ToString();
-            }
+
             dummyRow["FirstName"] = firstName;
             dummyRow["LastName"] = lastName;
             dummyRow["InTime"] = DateTime.Now.ToString("t");
@@ -393,8 +376,8 @@ namespace KumIn_WPF
             dummyRow["Barcode"] = barcode;
 
 
-            string tempRange = "Sheet1!A1:Z1000";
-            values = getSpreadsheetInfo(spreadsheetId2, tempRange);
+            string range2 = "Sheet1!A1:Z1000";
+            values = getSpreadsheetInfo(spreadsheetId2, range2);
 
             int pasteRowNum = 1;
             foreach (var row in values)
@@ -405,11 +388,12 @@ namespace KumIn_WPF
                 }
                 pasteRowNum++;
             }
+
             // edit this to pull appropriate valued from database into temp and record sheet columns
             if (lastName != null && firstName != null)
             {
                 range = "Sheet1!A" + rowNum.ToString() + ":Z" + rowNum.ToString();
-                tempRange = "Sheet1!A" + pasteRowNum.ToString() + ":Z" + pasteRowNum.ToString();
+                range2 = "Sheet1!A" + pasteRowNum.ToString() + ":Z" + pasteRowNum.ToString();
 
                 IList<IList<Object>> getValues = getSpreadsheetInfo(spreadsheetId, range);
 
@@ -421,7 +405,8 @@ namespace KumIn_WPF
                         oblist.Add(row[i]);
                 }
 
-                updateSpreadsheetInfo(oblist, spreadsheetId2, tempRange);
+                updateSpreadsheetInfo(oblist, spreadsheetId2, range2);
+
 
                 // inputs timeIn into temp spreadsheet
                 String range3 = "Sheet1!J" + pasteRowNum.ToString();
@@ -457,20 +442,23 @@ namespace KumIn_WPF
                 string lastName = (drv["LastName"]).ToString();
                 string email = "";
                 string range = "";
-                string spreadsheetId = "1Lxn9qUxUbNWt3cI70CuTEIxCfgpxjAlZPd6ARph4oCM";
+                string spreadsheetId = "1KmeqPF07jnjZJ_FwWgZ4taj9N3c-3HK7qvOJmG1TpWs";
                 /*
                 var user = (from u in db.FStudentTables
                             where u.FirstName == firstName && u.LastName == lastName
                             select u).FirstOrDefault();
                             */
-                IList<IList<Object>> values = getSpreadsheetInfo(spreadsheetId, "DB-Master!A1:AI");
+
+                IList<IList<Object>> values = getSpreadsheetInfo(spreadsheetId, "Sheet1!A1:J");
                 int rowNum = 1;
                 if (values != null && values.Count > 0)
                 {
+                    
+
                     foreach (var row in values)
                     {
-                        if (drv["FirstName"].ToString() == row[5].ToString()
-                        && drv["LastName"].ToString() == row[7].ToString())
+                        if (drv["FirstName"].ToString() == row[1].ToString()
+                        && drv["LastName"].ToString() == row[0].ToString())
                         {
                             break;
                         }
@@ -482,19 +470,12 @@ namespace KumIn_WPF
 
                 var myRow = values[rowNum - 1];
 
-                email = myRow[15].ToString();
+                email = myRow[3].ToString();
 
                 mail.From = new MailAddress("anthonyluukumon@gmail.com");
                 mail.To.Add(email); // reg email
                 mail.Subject = "Kumon HW notification";
-                mail.Body = "Dear KUMON Parents,\n Your child, " + drv["FirstName"].ToString() + " " + drv["LastName"].ToString() + 
-                    "attended center session today and turned in " + drv["#Completed"].ToString() +
-                    " assignment(s). We are still missing " + //drv["#Missing"] 
-                    " of his/her assignment(s).\n Per " + 
-                    "your request, this automated message is sent to notify you of the " +
-                    "missing homework. Although it's common that students will miss an assignment " +
-                    "from time to time due to various activities, we hope these notifications will " +
-                    "help you identify whether your child is chronically missing homework. \n Regards, \n KUMON San Ramon North \n 925-318-1628";
+                mail.Body = "Your student has not completed all of the homework assigned since last Kumon session.";
 
                 SmtpServer.Port = 587;
                 SmtpServer.Credentials = new System.Net.NetworkCredential("anthonyluukumon@gmail.com"
@@ -532,14 +513,15 @@ namespace KumIn_WPF
                 var user = (from u in db.FStudentTables
                             where u.FirstName == firstName && u.LastName == lastName
                             select u).FirstOrDefault();                                                          
-                */                
-                IList<IList<Object>>values = getSpreadsheetInfo("1Lxn9qUxUbNWt3cI70CuTEIxCfgpxjAlZPd6ARph4oCM", "Sheet1!A1:AI");
+                */
+                
+                IList<IList<Object>>values = getSpreadsheetInfo("1KmeqPF07jnjZJ_FwWgZ4taj9N3c-3HK7qvOJmG1TpWs", "Sheet1!A1:J");
 
                 int rowNum = 1;
                 foreach (var row in values)
                 {
-                    if (drv["FirstName"].ToString() == row[5].ToString()
-                        && drv["LastName"].ToString() == row[7].ToString())
+                    if (drv["FirstName"].ToString() == row[1].ToString()
+                        && drv["LastName"].ToString() == row[0].ToString())
                         break;
                     else
                         rowNum++;
@@ -547,10 +529,10 @@ namespace KumIn_WPF
 
                 var myRow = values[rowNum - 1];
 
-                phone1 = myRow[11].ToString();
-                carrier1 = myRow[12].ToString();
-                phone2 = myRow[17].ToString();
-                carrier2 = myRow[18].ToString();
+                phone1 = myRow[4].ToString();
+                carrier1 = myRow[5].ToString();
+                phone2 = myRow[6].ToString();
+                carrier2 = myRow[7].ToString();
 
                 if (carrier1 == "Verizon")
                 {
