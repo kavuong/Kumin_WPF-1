7b4da8bfee55228f63b27b180eb235b9de218d52
srinath <srinath99@gmail.com>
2016-12-09 17:40:20 -0800

    Color Converter for subjects

--- KumIn_WPF/ColorConverter.cs
+++ KumIn_WPF/ColorConverter.cs
@@ -8,29 +8,45 @@ using System.Globalization;
 
 namespace KumIn_WPF
 {
-    public class YellowConverter : IValueConverter
+    public class YellowConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            TimeSpan duration = TimeSpan.ParseExact(value.ToString(), "c", CultureInfo.InvariantCulture);
-            return duration.TotalMinutes >= 20 && duration.TotalMinutes < 30;
+            int min = 20;
+            int max = 30;
+
+            if (int.Parse(value[1].ToString()) == 2)
+            {
+                min = 40;
+                max = 60;
+            }
+
+            TimeSpan duration = TimeSpan.ParseExact(value[0].ToString(), "c", CultureInfo.InvariantCulture);
+            return duration.TotalMinutes >= min && duration.TotalMinutes < max;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
         {
             throw new Exception("Nope.");
         }
     }
 
-    public class RedConverter : IValueConverter
+    public class RedConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
+            int max = 30;
+
+            if (int.Parse(value[1].ToString()) == 2)
+            {
+                max = 60;
+            }
+
             TimeSpan duration = TimeSpan.ParseExact(value.ToString(), "c", CultureInfo.InvariantCulture);
-            return duration.TotalMinutes >= 30;
+            return duration.TotalMinutes >= max;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
         {
             throw new Exception("Nope.");
         }
