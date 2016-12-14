5918993ee6ce4b08f52fbe547967e1ec30491713
srinath <srinath99@gmail.com>
2016-12-13 22:05:53 -0800

    Done?????????forreal?

--- KumIn_WPF/HWEmails.xaml.cs
+++ KumIn_WPF/HWEmails.xaml.cs
@@ -39,17 +39,6 @@ namespace KumIn_WPF
             emailTable.Columns.Add("#Missing");
             emailTable.Columns.Add("Email");
 
-            int rowNum = emailConnection.getRowNum(ATTENDANCE_SHEET, ATTENDANCE_SHEET_RECORD + "A1:A"
-                , dpkDate.SelectedDate.Value.ToString("mm/dd/yyyy"));
-            IList<IList<Object>> desiredDate = emailConnection.get(ATTENDANCE_SHEET, ATTENDANCE_SHEET_RECORD
-                + "A" + rowNum.ToString() + ":B" + rowNum.ToString());
-
-
-
-
-
-
-
         }
 
 
@@ -57,22 +46,22 @@ namespace KumIn_WPF
 
         private void dpkDate_SelectedDateChanged(object sender, CalendarDateChangedEventArgs e)
         {
-            int rowNum = emailConnection.getRowNum(ATTENDANCE_SHEET, ATTENDANCE_SHEET_RECORD + "A1:A"
+            int rowNum = emailConnection.getRowNum(ATTENDANCE_SHEET, ATTENDANCE_SHEET_RECORD + "!A1:A"
                 , dpkDate.SelectedDate.Value.ToString("mm/dd/yyyy"));
             IList<IList<Object>> desiredDate = emailConnection.get(ATTENDANCE_SHEET, ATTENDANCE_SHEET_RECORD
-                + "A" + rowNum.ToString() + ":B" + rowNum.ToString());
+                + "!A" + rowNum.ToString() + ":B" + rowNum.ToString());
 
             if (desiredDate[0].Count == 1)
             {
-                // Populate with subsequent missing hw people
+                populate(rowNum + 1);
             }
             else if (desiredDate[0].Count == 2)
             {
-                // Already processed, notify
+                MessageBox.Show("Emails for this date have already been processed. Select a new date.");
             }
             else if (desiredDate[0] == null)
             {
-                // No students in record for selected date. Try again.
+                MessageBox.Show("No students in record for selected date. Try again.");
             }
         }
 
@@ -83,71 +72,95 @@ namespace KumIn_WPF
 
         private void btnSendEmails_Click(object sender, RoutedEventArgs e)
         {
+            for (int i = 0; i < emailTable.Rows.Count; i++)
+            {
+                CheckBox outBox = dgdEmails.Columns[4].GetCellContent(dgdEmails.Items[i]) as CheckBox;
 
+                if (outBox.IsChecked.Value)
+                {
+                    DataRowView drv = (DataRowView)dgdEmails.Items[i];
+                    sendHomeworkEmail(drv);
+                }
+
+            }
         }
 
 
 
 
-        /*
-        private void sendHomeworkEmail()
+
+        private void sendHomeworkEmail(DataRowView drv)
         {
             try
             {
                 MailMessage mail = new MailMessage();
                 SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-                //DataClasses1DataContext db = new DataClasses1DataContext();
 
-                DataRowView drv = (DataRowView)dgdListing.SelectedItem;
                 string firstName = (drv["FirstName"]).ToString();
                 string lastName = (drv["LastName"]).ToString();
-                string email = "";
-                string hwEmailVerif = "";
-                int rowNum = 1;
-               
-                            
+                string email = (drv["Email"]).ToString();
 
-                IList<IList<Object>> databaseValues = emailConnection.get(DATABASE_SHEET, DATABASE_SHEET_RECORD + "!A1:Z");
 
-                if (databaseValues != null && databaseValues.Count > 0)
-                {
-                    rowNum = emailConnection.getRowNum(DATABASE_SHEET, DATABASE_SHEET_RECORD +
-                        "!F1:F", firstName, DATABASE_SHEET_RECORD + "H1:H", lastName);
-                }
+                mail.From = new MailAddress("kumonsrn@gmail.com");
+                mail.To.Add(email); // reg email
+                mail.Subject = "Kumon HW notification";
+                mail.Body = "Dear KUMON Parents,\n Your child, " + drv["FirstName"].ToString() + " " + drv["LastName"].ToString() +
+                "attended center session today and turned in " + drv["#Completed"].ToString() +
+                " assignment(s). We are still missing " + drv["#Missing"].ToString() +
+                " of his/her assignment(s).\n Per " +
+                "your request, this automated message is sent to notify you of the " +
+                "missing homework. Although it's common that students will miss an assignment " +
+                "from time to time due to various activities, we hope these notifications will " +
+                "help you identify whether your child is chronically missing homework. \n\n Regards, \n KUMON San Ramon North \n 925-318-1628";
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("kumonsrn@gmail.com"
+                    , "letmeout");
+                SmtpServer.EnableSsl = true;
 
-                var myRow = databaseValues[rowNum - 1];
+                SmtpServer.Send(mail);
+                MessageBox.Show("HW Email Sent", "Success");
 
-                email = myRow[DATABASE_EMAIL].ToString();
-                hwEmailVerif = myRow[DATABASE_HW1_VERIF].ToString();
-
-                if (hwEmailVerif.ToUpper() == "YES")
-                {
-                    mail.From = new MailAddress("kumonsrn@gmail.com");
-                    mail.To.Add(email); // reg email
-                    mail.Subject = "Kumon HW notification";
-                    mail.Body = "Dear KUMON Parents,\n Your child, " + drv["FirstName"].ToString() + " " + drv["LastName"].ToString() +
-                    "attended center session today and turned in " + drv["#Completed"].ToString() +
-                    " assignment(s). We are still missing " + drv["#Missing"].ToString() +
-                    " of his/her assignment(s).\n Per " +
-                    "your request, this automated message is sent to notify you of the " +
-                    "missing homework. Although it's common that students will miss an assignment " +
-                    "from time to time due to various activities, we hope these notifications will " +
-                    "help you identify whether your child is chronically missing homework. \n\n Regards, \n KUMON San Ramon North \n 925-318-1628";
-                    SmtpServer.Port = 587;
-                    SmtpServer.Credentials = new System.Net.NetworkCredential("kumonsrn@gmail.com"
-                        , "letmeout");
-                    SmtpServer.EnableSsl = true;
-
-                    SmtpServer.Send(mail);
-                    MessageBox.Show("HW Email Sent", "Success");
-                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error");
             }
         } 
-        */
+
+
+
+
+
+
+
+        private void populate(int rowNum)
+        {
+            IList<IList<Object>> studentRecord = emailConnection.get(ATTENDANCE_SHEET
+                , ATTENDANCE_SHEET_RECORD + "!A" + rowNum.ToString() + ":AAA" + rowNum.ToString());
+
+            if (studentRecord != null)
+            {
+                if (studentRecord[0].Count > 2)
+                {
+                    string numMissing = studentRecord[0][10].ToString();
+
+                    if (numMissing != "" && int.Parse(numMissing) > 0)
+                    {
+                        DataRow newStudent = emailTable.NewRow();
+
+                        newStudent["FirstName"] = studentRecord[0][1].ToString();
+                        newStudent["LastName"] = studentRecord[0][0].ToString();
+                        newStudent["#Completed"] = studentRecord[0][9].ToString();
+                        newStudent["#Missing"] = studentRecord[0][10].ToString();
+                        newStudent["Email"] = studentRecord[0][3].ToString();
+
+                        emailTable.Rows.Add(newStudent);
+                    }
+
+                    populate(rowNum + 1);
+                }
+            }
+        }
     
     }
 }
